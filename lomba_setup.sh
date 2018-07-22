3c3e5fc0d36fb41eacec0cd93407cca5d6bc8cbb
Muhammad Muizzsuddin <arc.muiss@yahoo.com>
2018-07-22 10:26:41 +0700

    Update SH

---
+++ lomba_setup.sh
@@ -0,0 +1,12 @@
+echo 'Selamat datang di Lomba Logic Pondok Programmer'
+echo 'Anda akan mengunduh template Lomba Logic dari GitHub/moslog'
+echo 'Pastikan laptop anda terhubung ke internet'
+echo ''
+echo ''
+echo ''
+dir_lomba_logic='LombaLogic'
+rm -rf dir_lomba_logic
+mkdir dir_lomba_logic
+cd dir_lomba_logic
+curl -O https://raw.githubusercontent.com/moslog/logic-lomba-template-notebook/master/template.ipynb
+jupyter notebook
